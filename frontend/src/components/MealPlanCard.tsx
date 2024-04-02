d407ea1c111fba69471548e4a56ac58df715e377
jaydenpiao <jaydenpiao@gmail.com>
2024-04-01 21:00:16 -0700

    meal plan card

---
+++ frontend/src/components/MealPlanCard.tsx
@@ -0,0 +1,17 @@
+const MealPlanCard = ({ mealplan, onRecipesClick, onShoppingListClick }) => {
+  // Placeholder functions for button clicks
+  const handleDeleteMealPlanClick = () => alert("Deleted Meal Plan");
+
+  return (
+    <div className="border p-4 m-2 grid grid-cols-6">
+      <h2>{mealplan.name}</h2>
+      <button onClick={() => onRecipesClick(mealplan)}>Recipes</button>
+      <button onClick={() => onShoppingListClick(mealplan)}>
+        Shopping List
+      </button>
+      <button onClick={handleDeleteMealPlanClick}>Delete Meal Plan</button>
+    </div>
+  );
+};
+
+export default MealPlanCard;
