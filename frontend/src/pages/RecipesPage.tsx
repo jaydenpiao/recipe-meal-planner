d7f0bd560517a4b8217933fbc9f442d099fad94a
jaydenpiao <jaydenpiao@gmail.com>
2024-04-01 17:02:58 -0700

    display recipes on recipe page

--- frontend/src/pages/RecipesPage.tsx
+++ frontend/src/pages/RecipesPage.tsx
@@ -1,7 +1,45 @@
+import RecipeCard from "@/components/RecipeCard";
+
 const RecipesPage = () => {
+  const recipes = [
+    {
+      name: "Spaghetti",
+      rating: 4,
+      reviews: 2,
+      recipe: "1. cheese 2. sauce 3. pasta",
+      nutritionInfo: ["1000 calories", "10 fat", "1 protein"],
+    },
+    {
+      name: "Pizza",
+      rating: 4,
+      reviews: 2,
+      recipe: "1. cheese 2. sauce 3. pasta",
+      nutritionInfo: ["1000 calories", "10 fat", "1 protein"],
+    },
+    {
+      name: "Potatoes",
+      rating: 4,
+      reviews: 2,
+      recipe: "1. cheese 2. sauce 3. pasta",
+      nutritionInfo: ["1000 calories", "10 fat", "1 protein"],
+    },
+    {
+      name: "Burger",
+      rating: 4,
+      reviews: 2,
+      recipe: "1. cheese 2. sauce 3. pasta",
+      nutritionInfo: ["1000 calories", "10 fat", "1 protein"],
+    },
+  ];
+
   return (
-    <div className="flex justify-center">
-      <h1>Recipes Page</h1>
+    <div className="flex flex-col items-center overflow-auto">
+      <h1 className="text-lg font-bold">Recipes Page</h1>
+      <div className="w-full">
+        {recipes.map((recipe, index) => (
+          <RecipeCard key={index} recipe={recipe} />
+        ))}
+      </div>
     </div>
   );
 };
