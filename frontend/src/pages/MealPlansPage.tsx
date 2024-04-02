b6c56edb086d91bcc681d1a62d4910f3b3d395f7
jaydenpiao <jaydenpiao@gmail.com>
2024-04-01 21:16:47 -0700

    created meal plan page. still need recipes to link to new page and delete to work

--- frontend/src/pages/MealPlansPage.tsx
+++ frontend/src/pages/MealPlansPage.tsx
@@ -1,7 +1,59 @@
+import React, { useState } from "react";
+import ShoppingListOverlay from "@/components/ShoppingListOverlay";
+import MealPlanCard from "@/components/MealPlanCard";
+
 const MealPlansPage = () => {
+  const [isRecipeOverlayOpen, setRecipeOverlayOpen] = useState(false);
+  const [isShoppingListOverlayOpen, setShoppingListOverlayOpen] =
+    useState(false);
+  const [currentMealPlan, setCurrentMealPlan] = useState(null);
+
+  const mealplans = [
+    {
+      name: "Meal Plan 1",
+      recipes: ["Spaghetti", "Pizza"],
+      shoppingList: ["cheese", "sauce", "pasta", "dough"],
+    },
+    {
+      name: "Meal Plan 2",
+      recipes: ["Fries", "Ramen"],
+      shoppingList: ["potatoes", "noodles", "broth"],
+    },
+    {
+      name: "Meal Plan 3",
+      recipes: ["Fried Rice", "Poutine"],
+      shoppingList: ["rice", "potatoes", "gravy", "cheese"],
+    },
+  ];
+
+  const handleRecipesClick = (mealplan) => {
+    setCurrentMealPlan(mealplan);
+    // navigate to a new page with the recipes and url mealplans/{mealplan.name}
+  };
+
+  const handleShoppingListClick = (mealplan) => {
+    setCurrentMealPlan(mealplan);
+    setShoppingListOverlayOpen(true);
+  };
+
   return (
-    <div className="flex justify-center">
-      <h1>Meal Plans Page</h1>
+    <div className="flex flex-col items-center overflow-auto mt-24">
+      <h1 className="text-lg font-bold">Meal Plans Page</h1>
+      <div className="w-full">
+        {mealplans.map((mealplan, index) => (
+          <MealPlanCard
+            key={index}
+            mealplan={mealplan}
+            onRecipesClick={() => handleRecipesClick(mealplan)}
+            onShoppingListClick={() => handleShoppingListClick(mealplan)}
+          />
+        ))}
+        <ShoppingListOverlay
+          isOpen={isShoppingListOverlayOpen}
+          onClose={() => setShoppingListOverlayOpen(false)}
+          shoppingList={currentMealPlan?.shoppingList}
+        />
+      </div>
     </div>
   );
 };
