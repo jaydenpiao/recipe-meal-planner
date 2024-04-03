068fe00016280cd148d8abb504f9e7c67ac3ad80
jaydenpiao <jaydenpiao@gmail.com>
2024-04-03 12:24:18 -0700

    simple input and search button

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,9 +1,22 @@
-const SearchBar = () => {
-    return (
-        <div className="w-full ">
-        <input type="text" placeholder="Search..." />
-        </div>
-    );
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export function InputWithButton() {
+  return (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input type="email" placeholder="Email" />
+      <Button type="submit">Subscribe</Button>
+    </div>
+  );
 }
+
+const SearchBar = () => {
+  return (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input type="search" placeholder="Search for a recipe" />
+      <Button type="submit">Search</Button>
+    </div>
+  );
+};
 
 export default SearchBar;
