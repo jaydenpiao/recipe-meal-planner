6ca44ee9d9585b018b1ba5279b1f590a0cc74aa3
jaydenpiao <jaydenpiao@gmail.com>
2024-04-02 22:06:06 -0700

    mealplanspage.tsx done

--- frontend/src/pages/MealPlansPage.tsx
+++ frontend/src/pages/MealPlansPage.tsx
@@ -38,27 +38,6 @@ const MealPlansPage = () => {
     setShoppingListOverlayOpen(true);
   };
 
-  //   return (
-  //     <div className="flex flex-col items-center overflow-auto mt-24">
-  //       <h1 className="text-lg font-bold">Meal Plans Page</h1>
-  //       <div className="w-full">
-  //         {mealplans.map((mealplan, index) => (
-  //           <MealPlanCard
-  //             key={index}
-  //             mealplan={mealplan}
-  //             onRecipesClick={() => handleRecipesClick(mealplan)}
-  //             onShoppingListClick={() => handleShoppingListClick(mealplan)}
-  //           />
-  //         ))}
-  //         <ShoppingListOverlay
-  //           isOpen={isShoppingListOverlayOpen}
-  //           onClose={() => setShoppingListOverlayOpen(false)}
-  //           shoppingList={currentMealPlan?.shoppingList}
-  //         />
-  //       </div>
-  //     </div>
-  //   );
-
   return (
     <div className="flex flex-col items-center overflow-auto mt-24">
       <h1 className="text-lg font-bold">Meal Plans Page</h1>
@@ -71,65 +50,19 @@ const MealPlansPage = () => {
             onShoppingListClick={() => handleShoppingListClick(mealplan)}
           />
         ))}
+        <RecipesOverlay
+          mealPlanID={currentMealPlan?.mealPlanID}
+          isOpen={isRecipesOverlayOpen}
+          onClose={() => setRecipesOverlayOpen(false)}
+        />
+        <ShoppingListOverlay
+          mealPlanID={currentMealPlan?.mealPlanID}
+          isOpen={isShoppingListOverlayOpen}
+          onClose={() => setShoppingListOverlayOpen(false)}
+        />
       </div>
     </div>
   );
 };
 
 export default MealPlansPage;
-
-// const MealPlansPage = () => {
-//   const [isShoppingListOverlayOpen, setShoppingListOverlayOpen] =
-//     useState(false);
-//   const [currentMealPlan, setCurrentMealPlan] = useState(null);
-
-//   const mealplans = [
-//     {
-//       name: "Meal Plan 1",
-//       recipes: ["Spaghetti", "Pizza"],
-//       shoppingList: ["cheese", "sauce", "pasta", "dough"],
-//     },
-//     {
-//       name: "Meal Plan 2",
-//       recipes: ["Fries", "Ramen"],
-//       shoppingList: ["potatoes", "noodles", "broth"],
-//     },
-//     {
-//       name: "Meal Plan 3",
-//       recipes: ["Fried Rice", "Poutine"],
-//       shoppingList: ["rice", "potatoes", "gravy", "cheese"],
-//     },
-//   ];
-
-//   const handleRecipesClick = (mealplan) => {
-//     setCurrentMealPlan(mealplan);
-//   };
-
-//   const handleShoppingListClick = (mealplan) => {
-//     setCurrentMealPlan(mealplan);
-//     setShoppingListOverlayOpen(true);
-//   };
-
-//   return (
-//     <div className="flex flex-col items-center overflow-auto mt-24">
-//       <h1 className="text-lg font-bold">Meal Plans Page</h1>
-//       <div className="w-full">
-//         {mealplans.map((mealplan, index) => (
-//           <MealPlanCard
-//             key={index}
-//             mealplan={mealplan}
-//             onRecipesClick={() => handleRecipesClick(mealplan)}
-//             onShoppingListClick={() => handleShoppingListClick(mealplan)}
-//           />
-//         ))}
-//         <ShoppingListOverlay
-//           isOpen={isShoppingListOverlayOpen}
-//           onClose={() => setShoppingListOverlayOpen(false)}
-//           shoppingList={currentMealPlan?.shoppingList}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MealPlansPage;
