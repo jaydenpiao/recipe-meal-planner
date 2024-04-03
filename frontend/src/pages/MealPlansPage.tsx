aeee2cbb5c8cda9a8ef616d1722c8e92336845da
jaydenpiao <jaydenpiao@gmail.com>
2024-04-02 21:59:14 -0700

    meal plan cards work

--- frontend/src/pages/MealPlansPage.tsx
+++ frontend/src/pages/MealPlansPage.tsx
@@ -2,15 +2,78 @@ import React, { useState, useEffect } from "react";
 import ShoppingListOverlay from "@/components/ShoppingListOverlay";
 import MealPlanCard from "@/components/MealPlanCard";
 import { useUser } from "@/context/UserContext";
+import axios from "axios";
 
 const MealPlansPage = () => {
   const { selectedUserID } = useUser();
+  const [isRecipesOverlayOpen, setRecipesOverlayOpen] = useState(false);
+  const [isShoppingListOverlayOpen, setShoppingListOverlayOpen] =
+    useState(false);
+  const [currentMealPlan, setCurrentMealPlan] = useState(null);
+  const [mealplans, setMealPlans] = useState([]);
+
+  const getMealPlans = async (mealplan) => {
+    try {
+      console.log("Fetching mealplan for userID: ", selectedUserID);
+      const response = await axios.get(
+        `http://localhost:3000/api/mealplan/id/${selectedUserID}`
+      );
+      setMealPlans(response.data);
+    } catch (error) {
+      console.error("Error in MealPlansPage: ", error.message);
+    }
+  };
 
   useEffect(() => {
-    console.log("Selected userID: ", selectedUserID);
+    getMealPlans();
   }, [selectedUserID]);
 
-  return <div>mealplanspage</div>;
+  const handleRecipesClick = (mealplan) => {
+    setCurrentMealPlan(mealplan);
+    setRecipesOverlayOpen(true);
+  };
+
+  const handleShoppingListClick = (mealplan) => {
+    setCurrentMealPlan(mealplan);
+    setShoppingListOverlayOpen(true);
+  };
+
+  //   return (
+  //     <div className="flex flex-col items-center overflow-auto mt-24">
+  //       <h1 className="text-lg font-bold">Meal Plans Page</h1>
+  //       <div className="w-full">
+  //         {mealplans.map((mealplan, index) => (
+  //           <MealPlanCard
+  //             key={index}
+  //             mealplan={mealplan}
+  //             onRecipesClick={() => handleRecipesClick(mealplan)}
+  //             onShoppingListClick={() => handleShoppingListClick(mealplan)}
+  //           />
+  //         ))}
+  //         <ShoppingListOverlay
+  //           isOpen={isShoppingListOverlayOpen}
+  //           onClose={() => setShoppingListOverlayOpen(false)}
+  //           shoppingList={currentMealPlan?.shoppingList}
+  //         />
+  //       </div>
+  //     </div>
+  //   );
+
+  return (
+    <div className="flex flex-col items-center overflow-auto mt-24">
+      <h1 className="text-lg font-bold">Meal Plans Page</h1>
+      <div className="w-full">
+        {mealplans.map((mealplan) => (
+          <MealPlanCard
+            key={mealplan.mealPlanID}
+            mealplan={mealplan}
+            onRecipesClick={() => handleRecipesClick(mealplan)}
+            onShoppingListClick={() => handleShoppingListClick(mealplan)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MealPlansPage;
