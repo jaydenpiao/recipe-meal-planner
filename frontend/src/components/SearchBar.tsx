f96b97ccc106905174dd674af33705d1071bd27d
jaydenpiao <jaydenpiao@gmail.com>
2024-04-03 12:09:02 -0700

    add input component

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,9 @@
+const SearchBar = () => {
+    return (
+        <div className="w-full ">
+        <input type="text" placeholder="Search..." />
+        </div>
+    );
+}
+
+export default SearchBar;
